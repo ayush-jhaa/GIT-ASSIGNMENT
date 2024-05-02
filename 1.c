3bb8f8dd1a0c438beee143c03869439ba874ad7d
ayush-jhaa <ayushjha4112@gmail.com>
2024-05-02 11:14:59 +0530

    fixing bug of 1.c

--- 1.c
+++ 1.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main() {
 	int x = 5;
-	int y = 0;
+//	int y = 0;       for fixing bug we have to change the y to some value other than zero
+	
+	int y=1;
 	int result = x / y;
 	printf("Result: %d\n", result);
 	return 0;
